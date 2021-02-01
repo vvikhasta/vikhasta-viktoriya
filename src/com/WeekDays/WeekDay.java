f63f37ac1986fb6a7d296edaeb5cdb5ef3fefacf
vvikhasta <vvikhasta@gmail.com>
2021-02-01 12:55:25 +0200

    homework1

---
+++ src/com/WeekDays/WeekDay.java
@@ -0,0 +1,12 @@
+package com.hometask1.WeekDays;
+
+public enum WeekDay {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    END
+}
---
+++ src/com/WeekDays/WeekDayHomeTask.java
@@ -0,0 +1,71 @@
+package com.hometask1.WeekDays;
+import java.util.Scanner;
+
+public class WeekDayHomeTask {
+    public static void main(String[] args) {
+        boolean endType = false;
+        WeekDay inputDay = WeekDay.MONDAY;
+
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Let us begin...");
+
+        while (!endType) {
+            System.out.print("  Input any day of the week to see next day or end to exit: ");
+
+            boolean validInput;
+            do {
+
+                String inDay = in.nextLine().toLowerCase();
+                validInput = true;
+                switch (inDay) {
+                    case "monday": inputDay = WeekDay.MONDAY; break;
+                    case "tuesday": inputDay = WeekDay.TUESDAY; break;
+                    case "wednesday": inputDay = WeekDay.WEDNESDAY; break;
+                    case "thursday": inputDay = WeekDay.THURSDAY; break;
+                    case "friday": inputDay = WeekDay.FRIDAY; break;
+                    case "saturday": inputDay = WeekDay.SATURDAY; break;
+                    case "sunday": inputDay = WeekDay.SUNDAY; break;
+                    case "end": inputDay = WeekDay.END; break;
+
+
+                    default:
+                        System.out.println("  Invalid input, try again...");
+                        System.out.print("  Input any day of the week to see next day or end to exit: ");
+                        validInput = false;
+                }
+            } while (!validInput);
+
+
+
+                // Check result
+                if (inputDay == WeekDay.MONDAY) {
+                    System.out.println("Next day is Tuesday");
+
+                } else if (inputDay == WeekDay.TUESDAY) {
+                    System.out.println("Next day is Wednesday");
+
+                } else if (inputDay == WeekDay.WEDNESDAY) {
+                    System.out.println("Next day is Thursday");
+
+                } else if (inputDay == WeekDay.THURSDAY) {
+                    System.out.println("Next day is Friday");
+
+                } else if (inputDay == WeekDay.FRIDAY) {
+                    System.out.println("Next day is Saturday");
+
+                } else if (inputDay == WeekDay.SATURDAY) {
+                    System.out.println("Next day is Sunday");
+
+                } else if (inputDay == WeekDay.SUNDAY) {
+                    System.out.println("Next day is Monday");
+
+                }
+                else if (inputDay == WeekDay.END) {
+                    System.out.println("  Bye!");
+                    endType = true;
+                }
+            }
+        }
+
+    }
